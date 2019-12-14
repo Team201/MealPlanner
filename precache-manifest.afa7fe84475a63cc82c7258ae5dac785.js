2d3d4d388f080a128e09c17816780720dd70e68f
Yuriy <mrlazgen@gmail.com>
2019-12-14 15:38:02 +0700

    Updates

---
+++ precache-manifest.afa7fe84475a63cc82c7258ae5dac785.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8069a45e36fa43983e28ff08ff9746a5",
+    "url": "/MealPlanner/index.html"
+  },
+  {
+    "revision": "6402498b660d0b9fe904",
+    "url": "/MealPlanner/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "37e62ec860445a9923e3",
+    "url": "/MealPlanner/static/css/main.439b4a20.chunk.css"
+  },
+  {
+    "revision": "6402498b660d0b9fe904",
+    "url": "/MealPlanner/static/js/2.01f0d992.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/MealPlanner/static/js/2.01f0d992.chunk.js.LICENSE"
+  },
+  {
+    "revision": "37e62ec860445a9923e3",
+    "url": "/MealPlanner/static/js/main.1632112d.chunk.js"
+  },
+  {
+    "revision": "53e430847aca39fb9236",
+    "url": "/MealPlanner/static/js/runtime-main.98b2c53d.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/MealPlanner/static/media/persik.4e1ec840.png"
+  }
+]);
